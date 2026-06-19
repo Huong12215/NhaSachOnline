b96f647f574e358293ee3c149f3bd9cebda885e7
Huong12215 <ptthuong1314@gmail.com>
2026-06-20 03:50:35 +0700

    Cap nhat database context va model nha sach

---
+++ Data/DataSeed.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NhaSachOnline.Const;
+namespace NhaSachOnline.Data
+{
+    public class DataSeed
+    {
+        public static async Task KhoiTaoDuLieuMacDinh(IServiceProvider dichVu)
+        {
+            var quanLyNguoiDung = dichVu.GetService<UserManager<IdentityUser>>();
+            var quanLyVaiTro = dichVu.GetService<RoleManager<IdentityRole>>();
+
+            // Thêm 1 vai trò vào csdl
+            await quanLyVaiTro.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await quanLyVaiTro.CreateAsync(new IdentityRole(Roles.User.ToString()));
+
+            // Tạo thông tin mặc định cho tài khoản Admin 
+            // bao gồm: UserName, Email, xác thực email
+            var quanTri = new IdentityUser
+            {
+                UserName = "admin@test.xyz",
+                Email = "admin@test.xyz",
+                EmailConfirmed = true
+            };
+            var nguoiDungTrongCsdl = await quanLyNguoiDung.FindByEmailAsync(quanTri.Email);
+            // Nếu tài khoản của admin không tồn tại trong csdl 
+            // hoặc có thể hiểu là chưa có trong csdl
+            if (nguoiDungTrongCsdl is null)
+            {
+                // Tạo tài khoản admin với mk Admin12345!@#$%
+                var ketQua = await quanLyNguoiDung.CreateAsync(quanTri, "Admin12345!@#$%");
+                //var ketQua = await quanLyNguoiDung.CreateAsync(quanTri, "123");
+
+                // Nếu tạo thành công thì thêm vai trò người dùng 
+
+                if (ketQua.Succeeded)
+                {
+                    await quanLyNguoiDung.AddToRoleAsync(quanTri, Roles.Admin.ToString());
+                }
+                else
+                {
+                    // In ra mã lỗi 
+                    foreach(var error in ketQua.Errors)
+                    {
+                        Console.WriteLine(error.Description);
+                    }
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NhaSachOnline.Data;
+//using Microsoft.AspNetCore.Identity.UI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,23 +11,31 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services
+    .AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+//using (var scope = app.Services.CreateScope())
+//{
+    
+//    await DataSeed.KhoiTaoDuLieuMacDinh(scope.ServiceProvider);
+//}
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
